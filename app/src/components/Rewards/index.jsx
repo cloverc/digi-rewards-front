fab3574fceadcc024aafb5af7e921dc8e531d0c2
Clover Chapman <clo@cloverseeds.co.uk>
2018-07-17 16:16:24 +0100

    wheel of death added to Rewards component

--- app/src/components/Rewards/index.jsx
+++ app/src/components/Rewards/index.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Account from '../Account';
 import Reward from '../Reward';
 import './style.scss';
-// import Loader from '../Loader';
+import Loader from '../Loader';
 
 class Rewards extends Component {
 
@@ -11,40 +11,50 @@ class Rewards extends Component {
     super();
     this.state = {
       rewards: [],
+      loading: true,
     };
   }
 
   componentDidMount() {
     axios.get('http://127.0.0.1:3000/transaction')
       .then((response) => {
-        this.setState({ rewards: response.data });
-        console.log('merchantID: ', this.state.rewards);
+        this.setState({
+          rewards: response.data,
+          loading: false,
+        });
+        // console.log('merchantID: ', this.state.rewards);
       })
       .catch(error => console.log(error));
   }
 
   render() {
     const {
-      rewards,
+      rewards, loading,
     } = this.state;
 
     return (
       <div className="App">
-        <Account />
-        <div className="rewards__container">
-          <ul className="rewards rewards__list">
-            {
-              rewards.map(reward => (
-                <Reward
-                  key={reward._id}
-                  description={reward.description}
-                  logo={reward.merchant_logo}
-                  count={reward.count}
-                />
-              ))
-            }
-          </ul>
-        </div>
+        {loading ? (
+          <Loader />
+        ) : (
+          <React.Fragment>
+            <Account />
+            <div className="rewards__container">
+              <ul className="rewards rewards__list">
+                {
+                rewards.map(reward => (
+                  <Reward
+                    key={reward._id}
+                    description={reward.description}
+                    logo={reward.merchant_logo}
+                    count={reward.count}
+                  />
+                ))
+                }
+              </ul>
+            </div>
+          </React.Fragment>
+        )}
       </div>
     );
   }
